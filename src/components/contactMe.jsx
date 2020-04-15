022beae7d37712ecbd8d2d73fe3d7defca9f7576
mkjorl <43691578+mkjorl@users.noreply.github.com>
2020-04-14 22:26:26 -0600

    working contact form

--- src/components/contactMe.jsx
+++ src/components/contactMe.jsx
@@ -1,17 +1,95 @@
-import React from 'react';
-import { PageHeader } from 'antd';
+import React, { useState, useForm} from 'react';
+import { PageHeader, Form, Input, Button } from 'antd';
+import { LinkedinOutlined } from '@ant-design/icons';
+import axios from 'axios';
 
 const ContactMe = (props) => {
 
     let changeView = props.changeView;
+
+    const [serverState, setServerState] = useState({
+        submitting: false,
+        status: null
+    });
+
+    const [submitted, setSubmitted ] = useState(true);
+
+    const handleServerResponse = (ok) => {
+        setServerState({
+            submitting: false,
+            status: { ok }
+        });
+        if(ok) {
+            setSubmitted(true);
+        }
+    }
+
+    const onFinish = values => {
+        const data = new FormData();
+        data.append('name', values.name);
+        data.append('email', values.email);
+        data.append('message', values.message);
+        setServerState({ submitting: true});
+        axios({
+            method: 'POST',
+            url:"https://formspree.io/moqnrnqj",
+            data: data
+        })
+        .then(response => {
+            handleServerResponse(true);
+        })
+        .catch(response => {
+            handleServerResponse(false, response.response.data.error);
+            console.error(response.response.data.error);
+        })
+    };
 
     return(
         <div className="contact-main">
                <PageHeader
                 className="site-page-header"
                 onBack={() => changeView('home')}
-                title="Contact me"
+                title="Home"
             />
+
+            <div className="form-container">
+                
+                { submitted ?
+                    <div className="form-success-container">
+                        <h2>Thank-you!</h2>
+                        <p>I hope to speak to you soon.</p>
+                        <p>While you wait, connect with me on Linked<a href="https://www.linkedin.com/in/mark-kjorlien-0526a9185/" target="_blank"><LinkedinOutlined style={{fontSize: '2em'}}/></a></p>
+                    </div>
+                :
+                <Form className="contact-form"
+                    layout="vertical"
+                    onFinish={onFinish}>
+                        <h2>Contact Me</h2>
+                    <Form.Item label="Name" name="name"
+                        rules={[{
+                            required: true, message:'Please enter your name'
+                        }]}>
+                        <Input placeholder="Please enter your name"/>
+                    </Form.Item>
+                    <Form.Item label="Email" name="email"
+                        rules={[{
+                            type: 'email',
+                            required: true, message:'Please enter a valid email'
+                        }]}>
+                        <Input placeholder="Please enter your email"/>
+                    </Form.Item>
+                    <Form.Item label="Message" name="message"
+                        rules={[{
+                            required: true, message:'Please enter a message'
+                        }]}>
+                        <Input.TextArea className="message-box" placeholder="Please enter your message"/>
+                    </Form.Item>
+                    <Form.Item style={{paddingTop:30}}>
+                        <Button type="submit" htmlType="submit">Submit</Button>
+                    </Form.Item>
+                </Form>
+                }
+            </div>
         </div>
      
     );  
