39aa0f1bf1e0fd8a2c2c1045a5fc357ab262118f
mkjorl <43691578+mkjorl@users.noreply.github.com>
2020-04-14 22:26:49 -0600

    adjusted footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Row } from 'antd';
 
 const Footer = () => {
     return(
         <footer>
-            <p>made with</p>
-            <a href="https://firebase.google.com/" target="_blank"><img src="https://img.icons8.com/color/48/000000/firebase.png"/></a>
-            <a href="https://reactjs.org/" target="_blank"><img src="https://img.icons8.com/color/48/000000/react-native.png"/></a>
+            <Row className="footer-content">
+                <p>powered by</p>
+                <a href="https://reactjs.org/" target="_blank"><img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png"/></a> 
+            </Row>
+           
         </footer>
     );
 };
